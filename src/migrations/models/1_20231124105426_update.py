789479c183cdc726987c3e55993ecb57fc535f04
Roman Desyatskii <roman.desyatskii30@mail.ru>
2023-11-24 19:19:19 +0600

    Миграции

---
+++ src/migrations/models/1_20231124105426_update.py
@@ -0,0 +1,13 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        ALTER TABLE "article" ADD "author_id" UUID NOT NULL;
+        ALTER TABLE "article" ADD CONSTRAINT "fk_article_user_8c489e15" FOREIGN KEY ("author_id") REFERENCES "user" ("id") ON DELETE CASCADE;"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        ALTER TABLE "article" DROP CONSTRAINT "fk_article_user_8c489e15";
+        ALTER TABLE "article" DROP COLUMN "author_id";"""
